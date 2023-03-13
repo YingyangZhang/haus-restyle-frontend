51e7abff28175af0b77e237516b330e648e56c88
Yingyang Zhang <yingyang.zhang95@gmail.com>
2023-03-13 13:14:56 -0400

    added header component and header CSS

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import '../index.css';
+import { Route, Routes } from "react-router-dom";
+import Header from './header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
