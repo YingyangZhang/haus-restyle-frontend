26464ee3363aaed237b4afbe7ee64371d9a5c9d2
Yingyang Zhang <yingyang.zhang95@gmail.com>
2023-03-14 01:09:29 -0400

    added search

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,14 +3,30 @@ import { Route, Routes } from "react-router-dom";
 import Header from './header/Header';
 import Home from './home/Home';
 import Furnitures from './furnitures/Furnitures';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [furnitures, setFurnitures] = useState([]);
+  const [searchResult, setSearchResult] = useState(furnitures);
+
+  useEffect(() => {
+    fetch('https://haus-backend-hde3.onrender.com/furnitures')
+    .then(r => r.json())
+    .then(data => {
+        console.log(data);
+        const ascendingFurnitures = [...data].sort((a, b) =>
+        a.name > b.name ? -1 : 1);
+        setFurnitures(ascendingFurnitures);
+        setSearchResult(ascendingFurnitures);
+    })
+  },[])
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/furnitures' element={<Furnitures />} />
+        <Route path='/furnitures' element={<Furnitures furnitures={furnitures} setFurnitures={setFurnitures} searchResult={searchResult} setSearchResult={setSearchResult}/>} />
       </Routes>
     </div>
   )
--- src/Components/furnitures/Furnitures.js
+++ src/Components/furnitures/Furnitures.js
@@ -1,37 +1,89 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
+import Search from "./Search";
+import Footer from "../footer/Footer";
 
-export default function Furnitures() {
-    const [selectedCat, setSelectedCat] = useState('all');
+export default function Furnitures({furnitures, setFurnitures, searchResult, setSearchResult}) {
+    const [selectedCat, setSelectedCat] = useState('All');
+    const [isSearch, setIsSearch] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const filteredFurnitures = searchResult.filter(furniture => {
+        return selectedCat === "All" ? furniture : furniture.category.category_name === selectedCat;
+    });
+
+    useEffect(() => {
+        const handlePopstate = () => {
+          window.location.reload();
+        };
+      
+        window.addEventListener('popstate', handlePopstate);
+      
+        return () => {
+          window.removeEventListener('popstate', handlePopstate);
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollY = window.scrollY;
+          setIsScrolled(scrollY >= 3);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+      }, []);
 
     function handleCat(e) {
         setSelectedCat(e.target.value);
     }
 
     return (
-        <div className="furnitures-container grey-background">
-            <div className="furnitures-operations-container">
+        <div className="furnitures-container flex-box">
+            <div className={`furnitures-operations-container flex-box grey-background ${isScrolled ? 'add-dropshadow' : ''}`}>
                 <div className="cat-selections-container flex-box">
                     <label className="cat-selection">
-                        <input type="radio" name='selection' value="all" defaultChecked={true} onChange={handleCat} />
+                        <input type="radio" name='selection' value="All" defaultChecked={true} onChange={handleCat} />
                         <span>All</span>
                     </label>
 
                     <label className="cat-selection">
-                        <input type="radio" name='selection' value="chair" onChange={handleCat} />
+                        <input type="radio" name='selection' value="Chair" onChange={handleCat} />
                         <span>Chair</span>
                     </label>
 
                     <label className="cat-selection">
-                        <input type="radio" name='selection' value="table" onChange={handleCat} />
+                        <input type="radio" name='selection' value="Table" onChange={handleCat} />
                         <span>Table</span>
                     </label>
 
                     <label className="cat-selection">
-                        <input type="radio" name='selection' value="sofa" onChange={handleCat} />
+                        <input type="radio" name='selection' value="Sofa" onChange={handleCat} />
                         <span>Sofa</span>
                     </label>
                 </div>
+
+                <i className='bx bx-search' onClick={() => setIsSearch(true)}></i>
             </div>
+
+            <div className='cards-container'>
+                {filteredFurnitures.map(furniture => {
+                    return (
+                        <div className='card' key={furniture.id}>
+                            <div className='card-img-container'>
+                                <img src={furniture.image.thumbnail} alt='image' />
+                            </div>
+                            <div className='card-headline flex-box'>
+                                <p>{furniture.designer}</p>
+                                <p>{furniture.name}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+
+            <div className='furnitures-footer-container'>
+                <Footer />
+            </div>
+
+            {isSearch ? <Search setIsSearch={setIsSearch} furnitures={furnitures} setFurnitures={setFurnitures} setSearchResult={setSearchResult}/> : ''}
         </div>
     )
 }
